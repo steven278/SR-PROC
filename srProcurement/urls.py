f5f40838a4207f0bf0ae5cf92325648c25d5ba7e
steven278 <darkavenger347@gmail.com>
2022-12-01 14:42:11 +0700

    login feature test

--- srProcurement/urls.py
+++ srProcurement/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include #include is new 
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("django.contrib.auth.urls")),  # new (for login)
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
 ]
